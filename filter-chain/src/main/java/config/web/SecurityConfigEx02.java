7c72051781a6989258e2a6a22fd8ea4b819e8010
Gimun Lee <wlrn0514@gmail.com>
2025-01-15 23:59:48 +0900

    제목수정, 프로필 수정

--- filter-chain/src/main/java/config/web/SecurityConfigEx02.java
+++ filter-chain/src/main/java/config/web/SecurityConfigEx02.java
@@ -1,15 +1,35 @@
 package config.web;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.util.AntPathMatcher;
+
 import filter.SecurityFilterEx01;
 import filter.SecurityFilterEx02;
 import filter.SecurityFilterEx03;
 import filter.SecurityFilterEx04;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import jakarta.servlet.Filter;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 public class SecurityConfigEx02 {
-    @Bean
+	@Bean
+	public FilterChainProxy securityFilterChainProxy() {
+		List<SecurityFilterChain> securityFilterChains = Arrays.asList(
+				new DefaultSecurityFilterChain(new AntPathRequestMatcher("/hello/**"), securityFilterEx01(),securityFilterEx02()),
+				new DefaultSecurityFilterChain(new AntPathRequestMatcher("/ping/**"), securityFilterEx03(),securityFilterEx04())
+			);			
+		return new FilterChainProxy(securityFilterChains);
+	}
+	
+	@Bean
     public SecurityFilterEx01 securityFilterEx01() {
         return new SecurityFilterEx01();
     }
